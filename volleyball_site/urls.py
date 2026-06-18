c68016e127ba5daee35d3eabffbdb011de5027b0
Tergix <tergix@mail.ru>
2026-06-18 08:57:22 +0300

    fix urls

--- volleyball_site/urls.py
+++ volleyball_site/urls.py
@@ -1,4 +1,3 @@
-@"
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.views.static import serve
@@ -9,4 +8,3 @@ urlpatterns = [
     path('logout/', views.user_logout, name='logout'),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-"@ | Out-File -Encoding utf8 C:\Users\Sait\Desktop\d\volleyball-project-main\volleyball_site\urls.py
