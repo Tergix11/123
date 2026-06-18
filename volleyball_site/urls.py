71c3c0c9bdb8f23a7b1836bd23778d308e34fb6c
Tergix <tergix@mail.ru>
2026-06-18 08:51:16 +0300

    fix media serving

--- volleyball_site/urls.py
+++ volleyball_site/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static
-from main import views
+from django.views.static import serve
+from django.urls import re_path
 
-urlpatterns = [
-    path('', include('main.urls')),
-    path('logout/', views.user_logout, name='logout'),
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
